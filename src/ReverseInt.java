6b94f9ef44ec9fe6894f2ece97b2f93a0f1ee82a
ICA109 <t.yin1@outlook.com>
2018-02-19 21:25:55 -0500

    Faster non-string approach

--- src/ReverseInt.java
+++ src/ReverseInt.java
@@ -6,7 +6,8 @@ public class ReverseInt {
         System.out.println(reverse(1534236469));    //Too large for int...
     }
 
-
+    //String-based approach (slower)
+    /*
     public static int reverse(int x) {
         String rev = "";
         int wasNegative = x < 0 ? -1 : 1;   //Nice use of ternary operator
@@ -21,6 +22,21 @@ public class ReverseInt {
         }
         catch (NumberFormatException ex) {
             return  0;
+        }
+    }
+    */
+
+    //int-based approach. Faster
+    public static int reverse(int x) {
+        int output = 0;
+        while (true) {
+            if (x == 0)
+                return x;
+            output = output * 10 + x % 10;
+            if ((x /= 10) == 0)
+                return output;
+            if (output > 214748364 || output < -214748364)  //int overflow
+                return 0;
         }
     }
 }
