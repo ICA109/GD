ff298fd9ab678083f3717e14817d90496dc3cc8e
ICA109 <t.yin1@outlook.com>
2018-05-23 13:43:20 -0700

    HackerRank: Insertion Sort P1

---
+++ src/InsertionSortP1.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class InsertionSortP1 {
+
+    // Complete the insertionSort1 function below.
+    static void insertionSort1(int n, int[] arr) {
+
+        int toInsert = arr[arr.length-1];
+
+        for (int i=arr.length-1; i>0; i--) {
+
+            if (arr[i-1] > toInsert) {
+                arr[i] = arr[i-1];
+                printArray(arr);
+            }
+            else {
+                arr[i] = toInsert;
+                printArray(arr);
+                break;
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 3};
+        insertionSort1(5, arr);
+    }
+}
